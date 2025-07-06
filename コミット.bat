8ac542f440c48b69358a08106bf7327c3ca46703
hino <riyekuaiyin@gmail.com>
2025-07-06 20:48:34 +0900

    Initial commit

---
+++ コミット.bat
@@ -0,0 +1,10 @@
+﻿@echo off
+chcp 65001 > nul
+cd /d %~dp0
+
+git add .
+git commit -m "基盤"
+git push origin master
+echo Push完了!GItHubに反映されました
+
+pause
---
+++ 起動.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d C:\AIagent\spaceIDLEgame
+python -m http.server &
+pause
